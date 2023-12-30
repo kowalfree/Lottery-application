b0d485b712150a3b2888d8f5e9adcba904429d2b
kowalfree <kowalfree@gmail.com>
2023-12-30 11:11:48 +0100

    I add funktion for calculate points and exit game

--- lottery.cpp
+++ lottery.cpp
@@ -1,63 +1,100 @@
 #include <iostream>
 #include <random>
-
+#include <vector>
 
 using namespace std;
 
-int num1, num2, num3, num4, num5, num6;
+int num [6];
+int *randomArray = new int[49]; //array reservation
+char play = {'Y'}; // variable for "Yes or Not?"
 
 // WRITE MY OWN NUMBERS
 
-void myNumbers () {
+void myNumbers () 
+{
+    cout<<"\nWrite your 6 numbers from 1 to 49. The numbers cannot be repeated."<< endl;
+    int i;
 
-cout<<"Write your 6 numbers from 1 to 49. The numbers cannot be repeated."<< endl;
-cin >> num1 >> num2 >> num3 >> num4 >> num5 >> num6; 
+    for (i = 0; i<6; i++) 
+    {
+        cin >> num[i]; 
+        // Condition for check range
+        if (num[i] < 1 || num[i] > 49) 
+        { 
+            cout<<"Number " << num[i] << " is not in the range 1 to 49. Try entering a different number."<< endl;
+            cin >> num[i]; 
+        } 
+        // Loop for check repeated numbers
+        for (int j = 0; j<6; j++) 
+        {
+            if (j != i) 
+            {
+                if (num[j] == num[i]) 
+                {
+                    cout<<"Number " << num[j] << " is repeated. Try entering a different number."<< endl;
+                    cin >> num[j]; 
+                }     
+            }              
+        }      
+    }
 
-if (num1 < 1 || num1 > 49) {cout<<"Number " << num1 << " is not in the range 1 to 49"<< endl;
-} else if (num2 < 1 || num2 > 49) {cout<<"Number " << num2 << " is not in the range 1 to 49"<< endl;
-} else if (num3 < 1 || num3 > 49) {cout<<"Number " << num3 << " is not in the range 1 to 49"<< endl;
-} else if (num4 < 1 || num4 > 49) {cout<<"Number " << num4 << " is not in the range 1 to 49"<< endl;
-} else if (num5 < 1 || num5 > 49) {cout<<"Number " << num5 << " is not in the range 1 to 49"<< endl;
-} else if (num6 < 1 || num6 > 49) {cout<<"Number " << num6 << " is not in the range 1 to 49"<< endl;
-} else {cout<<"You provided the following numbers: "<< num1 <<" "<< num2 <<" "<< num3 <<" "<< num4 <<" "<< num5 <<" "<< num6 << endl;}
+    // void repeat () 
+// {
+//     for (int i=0; i < 49; i++) 
+//     {   
+//         for (int j=0; j < 49; j++) 
+//         {  
+//             if (randomArray[i] == randomArray[j] && i!=j) 
+//             {
+//                 cout<<"repeat numbers: "<<randomArray[i]<<" ";
+//             }
+//         }
+// 	}
+// }
 
+    cout<<"\nYou entered the following numbers: ";
+     // Loop for print our numbers
+    for (int k = 0; k < 6; k++) {
+            
+        cout<< num[k] <<" ";
+    }
 }
 
 // RANDOM NUMBERS GENERATOR
 
-void numbersGenerator () {
-
+void numbersGenerator () 
+{
     random_device rd; // class is used to obtain a seed value for the random number generator
-    mt19937_64 randomEngine(rd()); // class is a random number generation engine that produces random numbers with a uniform distribution.
+    mt19937 randomEngine(rd()); // class is a random number generation engine that produces random numbers with a uniform distribution.
     uniform_int_distribution <int> distr(1, 49); // class is used to generate random integers within a given range
     bool checkNum; // variable for help
-    int *randomArray = new int[6]; //array reservation
     int howMany = {0}; // how many draws were made
-
+   
     // loop for randomizing numbers
-    for (int i=1; i<=6; i++) { 
-            int number = distr(randomEngine); // function for generate random numbers
-            checkNum = true; 
-
+    for (int i=1; i<=6; i++) 
+    { 
         do {
-            // loop to check for number repetitions
-            for (int j=1; j<=howMany; j++) { 
+            int number = distr(randomEngine); // function for generate random numbers
+            checkNum = {true};
 
+            // loop to check for number repetitions
+            for (int j=1; j<=howMany; j++) 
+            { 
                 // condition checking number repetitions
-                if (number == randomArray[j]) { 
+                if (number == randomArray[j-1]) 
+                { 
                     checkNum = false;
                 } 
             }
-            
             // a loop for entering the correct numbers into the array
-            if (checkNum == true) { 
+            if (checkNum == true) 
+            { 
+                randomArray[howMany] = number;
                 howMany++; 
-                randomArray[howMany-1] = number;
             }
-
         } while (checkNum != true);
     }
-    cout<<"Lottery numbers: ";
+    cout<<"\nLottery numbers: ";
     
     // loop to print the entire array
     for (int i=0; i < howMany; i++) 
@@ -66,12 +103,81 @@ void numbersGenerator () {
 	}
 }
 
+// FUNCTION FOR CALCULATE POINTS
+void points () 
+{   int i;
+    int j;
+    vector <int> sameNumbers; // VECTOR WITH SAME NUMBERS
+
+    // Loop for check and add same numbers to vector
+    for (i = 0; i<6; i++)
+    {
+         for (j = 0; j<6; j++)
+        {
+            if (num[i] == randomArray[j]) 
+            {
+                sameNumbers.push_back(num[i]);   
+            }
+        }   
+    }
+
+    // Condition for check amount of same numbers in vector
+    if (sameNumbers.empty() == true) // 
+    {
+        cout<<"\n\nI'm sorry, you didn't get any numbers. Do you wanna try again? [Y/N]  ";
+    } 
+    else if (sameNumbers.size() == 1)
+    {
+        cout<<"\n\nCongratulations, you got "<< sameNumbers.size() <<" number: "<< sameNumbers.at(0)<< ". Do you wanna try again? [Y/N]  ";
+    }
+    else 
+    {
+        cout<<"\n\nCongratulations, you got "<< sameNumbers.size() <<" numbers: "; 
+
+        for (size_t k = 0; k<sameNumbers.size(); k++)
+        {
+            cout << sameNumbers.at(k)<<" ";
+        };
+
+        cout<< ". Do you wanna try again? [Y/N]  ";
+    }
+}
+
+// FUNCTION FOR PLAY MORE OR QUIT GAME
+int tryAgain () 
+{
+    cin>> play;
+
+    if (play == 'Y' || play == 'y') 
+    {
+        myNumbers ();
+        numbersGenerator ();
+        points ();
+        tryAgain ();
+    }
+    else if (play == 'N' || play == 'n') 
+    {   
+       return 0;
+    }
+    else 
+    {
+        cout<<"\nWrong answer. Do you wanna try again? [Y/N] "; 
+        tryAgain ();
+    }
+    return 0;         
+}
+
 //MAIN FUNCTION
 
-int main () {
-
-myNumbers ();
-numbersGenerator ();
-
+int main () 
+{  
+    myNumbers ();
+    numbersGenerator ();
+    points ();
+    tryAgain ();
+    delete [] randomArray;
+    
     return 0;
 };
+
+
